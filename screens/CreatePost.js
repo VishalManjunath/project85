f8423c596b725a0276654e5368911b032b61a5ba
VishalManjunath <vishal15.dk@gmail.com>
2021-11-28 14:30:21 +0530

    first commit

---
+++ screens/CreatePost.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+export default class CreatePost extends React.Component {
+    render() {
+        return(
+            <View>
+                <Text>
+                    CreatePost
+                </Text>
+            </View>
+        );
+    }
+}
